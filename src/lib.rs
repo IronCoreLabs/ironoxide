9fbeab4b0e1d4da5de64101dff55b0640bf8fe8f
Clint Frederickson <clintfred@users.noreply.github.com>
2019-05-06 15:58:48 -0600

    Patches to the 0.6.0 release (#1)
    * restore `UserCreateKeyPair` to public API
    * added `IronOxideErr` to the `prelude`
    * added `From<IronOxideErr> for String` to lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -105,3 +105,10 @@ impl IronOxide {
         &self.device
     }
 }
+
+/// A way to turn IronSdkErr into Strings for the Java binding
+impl From<IronOxideErr> for String {
+    fn from(err: IronOxideErr) -> Self {
+        format!("{}", err)
+    }
+}
