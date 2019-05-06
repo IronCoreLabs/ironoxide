84540ae32ebf4c1fbc08e68562f79e91ddcf3778
Clint Frederickson <clint.frederickson@ironcorelabs.com>
2019-05-06 15:22:51 -0600

    merge From instance into lib.rs

--- src/binding.rs
+++
@@ -1,9 +0,0 @@
-pub mod java {
-    use crate::IronOxideErr;
-    /// A way to turn IronSdkErr into Strings for the Java binding
-    impl From<IronOxideErr> for String {
-        fn from(err: IronOxideErr) -> Self {
-            format!("{}", err)
-        }
-    }
-}
