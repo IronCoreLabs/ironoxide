dc9033ab4b97826cdf7bdc7b0d4a115f9684ab45
Clint Frederickson <clint.frederickson@ironcorelabs.com>
2019-08-01 15:30:09 -0600

    check format after proto source gen

--- .travis_scripts/cross-test.sh
+++ .travis_scripts/cross-test.sh
@@ -5,8 +5,8 @@ set -x
 
 # If no special target provided, use default cargo arch for building and run all unit tests
 if [ -z "$TARGET" ]; then
-    cargo fmt -- --check
     cargo t --verbose
+    cargo fmt -- --check
 # Cross doesn't have support for iOS builds, so use cargo to add the target and compile for it
 elif [ "$IOS" = 1 ]; then
     rustup target add "$TARGET"
