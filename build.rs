b349350c124b0a06e1b406426a264f9f60626012
Clint Frederickson <clint.frederickson@ironcorelabs.com>
2019-08-01 14:08:46 -0600

    #[21] build and readme updates

--- build.rs
+++ build.rs
@@ -3,7 +3,7 @@ extern crate protobuf_codegen_pure;
 fn main() {
     protobuf_codegen_pure::run(protobuf_codegen_pure::Args {
         out_dir: "src/proto",
-        input: &["proto/Foo.proto", "proto/edeks.proto"],
+        input: &["proto/edeks.proto"],
         includes: &["proto"],
         customize: protobuf_codegen_pure::Customize {
             carllerche_bytes_for_bytes: Some(true),
