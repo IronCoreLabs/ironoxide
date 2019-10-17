3267feb030e06654b7f7d1e1ec0fb0904db208d2
Craig Colegrove <craig.colegrove@ironcorelabs.com>
2019-10-17 16:34:49 -0600

    Added needs_rotation to several structures

--- src/internal/group_api/mod.rs
+++ src/internal/group_api/mod.rs
@@ -97,6 +97,7 @@ pub struct GroupMetaResult {
     is_member: bool,
     created: DateTime<Utc>,
     updated: DateTime<Utc>,
+    needs_rotation: Option<bool>,
 }
 impl GroupMetaResult {
     /// A single document grant/revoke failure for a user or group.
@@ -123,6 +124,9 @@ impl GroupMetaResult {
     pub fn group_master_public_key(&self) -> &PublicKey {
         &self.group_master_public_key
     }
+    pub fn needs_rotation(&self) -> Option<bool> {
+        self.needs_rotation
+    }
 }
 
 /// Group information.
@@ -137,6 +141,7 @@ pub struct GroupGetResult {
     member_list: Option<Vec<UserId>>,
     created: DateTime<Utc>,
     updated: DateTime<Utc>,
+    needs_rotation: Option<bool>,
     pub(crate) encrypted_private_key: Option<TransformedEncryptedValue>,
 }
 impl GroupGetResult {
@@ -171,6 +176,10 @@ impl GroupGetResult {
     /// List of group members. Members of a group can decrypt values encrypted to the group.
     pub fn member_list(&self) -> Option<&Vec<UserId>> {
         self.member_list.as_ref()
+    }
+    /// None if the calling user is not a group admin
+    pub fn needs_rotation(&self) -> Option<bool> {
+        self.needs_rotation
     }
 }
 
--- tests/group_ops.rs
+++ tests/group_ops.rs
@@ -18,7 +18,7 @@ fn group_create_no_member() {
         false,
     ));
 
-    assert!(group_result.is_ok())
+    assert_eq!(group_result.unwrap().needs_rotation(), Some(false))
 }
 
 #[test]
@@ -26,8 +26,28 @@ fn group_create_also_member() {
     let sdk = init_sdk();
 
     let group_result = sdk.group_create(&Default::default());
+    assert_eq!(group_result.unwrap().needs_rotation(), Some(false))
+}
 
-    assert!(group_result.is_ok())
+#[test]
+fn group_get_metadata() {
+    let admin_sdk = init_sdk();
+    let member_sdk = init_sdk();
+    let nonmember_sdk = init_sdk();
+
+    let member_id = member_sdk.device().account_id().clone();
+    let group = admin_sdk.group_create(&Default::default());
+    let group_id = &group.unwrap().id().clone();
+
+    let _ = admin_sdk.group_add_members(&group_id, &[member_id]);
+
+    let admin_group_get = admin_sdk.group_get_metadata(group_id).unwrap();
+    let member_group_get = member_sdk.group_get_metadata(group_id).unwrap();
+    let nonmember_group_get = nonmember_sdk.group_get_metadata(group_id).unwrap();
+
+    assert_eq!(admin_group_get.needs_rotation(), Some(false));
+    assert_eq!(member_group_get.needs_rotation(), None);
+    assert_eq!(nonmember_group_get.needs_rotation(), None);
 }
 
 #[test]
