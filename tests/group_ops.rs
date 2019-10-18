58da6ac562a763aa62b75c88b162e2a746982f0e
Craig Colegrove <craig.colegrove@ironcorelabs.com>
2019-10-18 11:00:44 -0600

    Used ? instead of unwrap in test

--- tests/group_ops.rs
+++ tests/group_ops.rs
@@ -30,24 +30,25 @@ fn group_create_also_member() {
 }
 
 #[test]
-fn group_get_metadata() {
+fn group_get_metadata() -> Result<(), IronOxideErr> {
     let admin_sdk = init_sdk();
     let member_sdk = init_sdk();
     let nonmember_sdk = init_sdk();
 
     let member_id = member_sdk.device().account_id().clone();
     let group = admin_sdk.group_create(&Default::default());
-    let group_id = &group.unwrap().id().clone();
+    let group_id = &group?.id().clone();
 
-    let _ = admin_sdk.group_add_members(&group_id, &[member_id]);
+    admin_sdk.group_add_members(&group_id, &[member_id])?;
 
-    let admin_group_get = admin_sdk.group_get_metadata(group_id).unwrap();
-    let member_group_get = member_sdk.group_get_metadata(group_id).unwrap();
-    let nonmember_group_get = nonmember_sdk.group_get_metadata(group_id).unwrap();
+    let admin_group_get = admin_sdk.group_get_metadata(group_id)?;
+    let member_group_get = member_sdk.group_get_metadata(group_id)?;
+    let nonmember_group_get = nonmember_sdk.group_get_metadata(group_id)?;
 
     assert_eq!(admin_group_get.needs_rotation(), Some(false));
     assert_eq!(member_group_get.needs_rotation(), None);
     assert_eq!(nonmember_group_get.needs_rotation(), None);
+    Ok(())
 }
 
 #[test]
