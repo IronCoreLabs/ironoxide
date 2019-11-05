789a0e0ca713c9e209d7af932f48348f63565095
Craig Colegrove <craig.colegrove@ironcorelabs.com>
2019-11-05 15:31:30 -0700

    Make Travis point to dev

--- .travis_scripts/cross-test.sh
+++ .travis_scripts/cross-test.sh
@@ -5,7 +5,7 @@ set -x
 
 # If no special target provided, use default cargo arch for building and run all unit tests
 if [ -z "$TARGET" ]; then
-    cargo t --verbose
+    TEST_ENV=dev cargo t --verbose
     cargo fmt -- --check
 # Cross doesn't have support for iOS builds, so use cargo to add the target and compile for it
 elif [ "$IOS" = 1 ]; then
