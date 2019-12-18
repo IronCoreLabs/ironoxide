0771da2de76fafc65260ed9de5238d5a9356fc41
Clint Frederickson <clint.frederickson@ironcorelabs.com>
2019-12-18 11:10:13 -0700

    [#93] converts user_ops and group_ops tests to be async compatible

--- tests/user_ops.rs
+++ tests/user_ops.rs
@@ -12,23 +12,24 @@ use uuid::Uuid;
 #[macro_use]
 extern crate serde_json;
 
-#[test]
-fn user_verify_non_existing_user() -> Result<(), IronOxideErr> {
-    let option_result = IronOxide::user_verify(&gen_jwt(None).0)?;
+#[tokio::test]
+async fn user_verify_non_existing_user() -> Result<(), IronOxideErr> {
+    let option_result = IronOxide::user_verify(&gen_jwt(None).0).await?;
     assert_eq!(true, option_result.is_none());
     Ok(())
 }
 
-#[test]
-fn user_verify_existing_user() -> Result<(), IronOxideErr> {
+#[tokio::test]
+async fn user_verify_existing_user() -> Result<(), IronOxideErr> {
     let account_id: UserId = create_id_all_classes("").try_into()?;
     IronOxide::user_create(
         &gen_jwt(Some(account_id.id())).0,
         "foo",
         &Default::default(),
-    )?;
+    )
+    .await?;
 
-    let result = IronOxide::user_verify(&gen_jwt(Some(account_id.id())).0)?;
+    let result = IronOxide::user_verify(&gen_jwt(Some(account_id.id())).0).await?;
     assert_eq!(true, result.is_some());
     let verify_resp = result.unwrap();
 
@@ -36,36 +37,39 @@ fn user_verify_existing_user() -> Result<(), IronOxideErr> {
     Ok(())
 }
 
-#[test]
-fn user_verify_after_create_with_needs_rotation() -> Result<(), IronOxideErr> {
+#[tokio::test]
+async fn user_verify_after_create_with_needs_rotation() -> Result<(), IronOxideErr> {
     let account_id: UserId = Uuid::new_v4().to_string().try_into()?;
     IronOxide::user_create(
         &gen_jwt(Some(account_id.id())).0,
         "foo",
         &UserCreateOpts::new(true),
-    )?;
+    )
+    .await?;
 
-    let result = IronOxide::user_verify(&gen_jwt(Some(account_id.id())).0)?;
+    let result = IronOxide::user_verify(&gen_jwt(Some(account_id.id())).0).await?;
     assert!(result.is_some());
     let verify_resp = result.unwrap();
     assert!(verify_resp.needs_rotation());
     Ok(())
 }
-#[test]
-fn user_create_good_with_devices() -> Result<(), IronOxideErr> {
+#[tokio::test]
+async fn user_create_good_with_devices() -> Result<(), IronOxideErr> {
     let account_id: UserId = Uuid::new_v4().to_string().try_into()?;
     IronOxide::user_create(
         &gen_jwt(Some(account_id.id())).0,
         "foo",
         &Default::default(),
-    )?;
+    )
+    .await?;
     let device = IronOxide::generate_new_device(
         &gen_jwt(Some(account_id.id())).0,
         "foo",
         &DeviceCreateOpts::new(Some("myDevice".try_into()?)),
-    )?;
-    let sdk = ironoxide::initialize(&device)?;
-    let device_list = sdk.user_list_devices()?;
+    )
+    .await?;
+    let sdk = ironoxide::initialize(&device).await?;
+    let device_list = sdk.user_list_devices().await?;
 
     assert_eq!(1, device_list.result().len());
     assert_eq!(
@@ -75,14 +79,14 @@ fn user_create_good_with_devices() -> Result<(), IronOxideErr> {
     Ok(())
 }
 
-#[test]
-fn user_private_key_rotation() -> Result<(), IronOxideErr> {
-    let io = initialize_sdk()?;
+#[tokio::test]
+async fn user_private_key_rotation() -> Result<(), IronOxideErr> {
+    let io = initialize_sdk().await?;
 
-    let result1 = io.user_rotate_private_key(common::USER_PASSWORD)?;
+    let result1 = io.user_rotate_private_key(common::USER_PASSWORD).await?;
     assert_eq!(result1.needs_rotation(), false);
 
-    let result2 = io.user_rotate_private_key(common::USER_PASSWORD)?;
+    let result2 = io.user_rotate_private_key(common::USER_PASSWORD).await?;
     assert_ne!(
         &result1.user_master_private_key(),
         &result2.user_master_private_key()
@@ -91,16 +95,16 @@ fn user_private_key_rotation() -> Result<(), IronOxideErr> {
     Ok(())
 }
 
-#[test]
-fn sdk_init_with_private_key_rotation() -> Result<(), IronOxideErr> {
+#[tokio::test]
+async fn sdk_init_with_private_key_rotation() -> Result<(), IronOxideErr> {
     use ironoxide::InitAndRotationCheck;
 
-    let (user_id, init_result) = common::init_sdk_get_init_result(true);
+    let (user_id, init_result) = common::init_sdk_get_init_result(true).await;
     let _: IronOxide = match init_result {
         InitAndRotationCheck::NoRotationNeeded(_ironoxide) => panic!("user should need rotation"),
         InitAndRotationCheck::RotationNeeded(io, rotation_check) => {
             assert_eq!(rotation_check.user_rotation_needed(), Some(&user_id));
-            let rotation_result = io.user_rotate_private_key(common::USER_PASSWORD)?;
+            let rotation_result = io.user_rotate_private_key(common::USER_PASSWORD).await?;
             assert_eq!(rotation_result.needs_rotation(), false);
             io
         }
@@ -108,31 +112,34 @@ fn sdk_init_with_private_key_rotation() -> Result<(), IronOxideErr> {
     Ok(())
 }
 
-#[test]
-fn user_add_device_after_rotation() -> Result<(), IronOxideErr> {
+#[tokio::test]
+async fn user_add_device_after_rotation() -> Result<(), IronOxideErr> {
     //create a user
-    let (user, sdk) = common::init_sdk_get_user();
+    let (user, sdk) = common::init_sdk_get_user().await;
     let bytes = vec![42u8, 43u8];
 
-    let encrypt_result = sdk.document_encrypt(
-        &bytes,
-        &DocumentEncryptOpts::with_explicit_grants(None, None, true, vec![]),
-    )?;
+    let encrypt_result = sdk
+        .document_encrypt(
+            &bytes,
+            &DocumentEncryptOpts::with_explicit_grants(None, None, true, vec![]),
+        )
+        .await?;
     let encrypted_data = encrypt_result.encrypted_data();
 
     //rotate the private key
-    let _rotation_result = sdk.user_rotate_private_key(common::USER_PASSWORD)?;
+    let _rotation_result = sdk.user_rotate_private_key(common::USER_PASSWORD).await?;
 
     //add a new device
     let new_device = IronOxide::generate_new_device(
         &common::gen_jwt(Some(user.id())).0,
         common::USER_PASSWORD,
         &Default::default(),
-    )?;
+    )
+    .await?;
 
     //reinitialize the sdk with the new device and decrypt some data
-    let new_sdk = ironoxide::initialize(&new_device)?;
-    let decrypt_result = new_sdk.document_decrypt(&encrypted_data)?;
+    let new_sdk = ironoxide::initialize(&new_device).await?;
+    let decrypt_result = new_sdk.document_decrypt(&encrypted_data).await?;
     let decrypted_data = decrypt_result.decrypted_data();
 
     assert_eq!(bytes, decrypted_data.to_vec());
@@ -140,14 +147,15 @@ fn user_add_device_after_rotation() -> Result<(), IronOxideErr> {
     Ok(())
 }
 
-#[test]
-fn user_create_with_needs_rotation() -> Result<(), IronOxideErr> {
+#[tokio::test]
+async fn user_create_with_needs_rotation() -> Result<(), IronOxideErr> {
     let account_id: UserId = Uuid::new_v4().to_string().try_into()?;
     let result = IronOxide::user_create(
         &gen_jwt(Some(account_id.id())).0,
         common::USER_PASSWORD,
         &UserCreateOpts::new(true),
-    );
+    )
+    .await;
     assert!(result?.needs_rotation());
     Ok(())
 }
