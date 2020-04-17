2f84184b4c200661d8b7520ed34d0d555361871f
Chris Jones <chris.jones@ironcorelabs.com>
2020-04-17 10:07:07 -0600

    Travis: Build several android architectures. (#137)

--- .travis_scripts/cross-test.sh
+++ .travis_scripts/cross-test.sh
@@ -5,7 +5,7 @@ set -x
 
 # If no special target provided, use default cargo arch for building and run all unit tests
 if [ -z "$TARGET" ]; then
-    IRONCORE_ENV=dev cargo t --features "blocking beta" --verbose
+    IRONCORE_ENV=dev cargo test --features "blocking beta" --verbose
     cargo fmt -- --check
 # Cross doesn't have support for iOS builds, so use cargo to add the target and compile for it
 elif [ "$IOS" = 1 ]; then
@@ -13,5 +13,5 @@ elif [ "$IOS" = 1 ]; then
     cargo build --features "blocking beta" --target "$TARGET"
 # For everything else, use cross to compile for the specified target
 else
-    cross build --features "blocking beta" --target "$TARGET"
+    cross build --no-default-features --features "blocking beta tls-vendored-openssl" --target "$TARGET"
 fi
