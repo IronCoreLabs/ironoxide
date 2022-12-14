79d6398c9110e3be73bf89842729b0bfea7d9c67
Craig Colegrove <craig.colegrove@ironcorelabs.com>
2022-12-14 11:38:56 -0800

    Make clippy happy I guess

--- src/internal/user_api.rs
+++ src/internal/user_api.rs
@@ -317,7 +317,7 @@ pub struct JwtClaims {
 ///
 /// Must be either ES256 or RS256 and have a payload similar to [JwtClaims](struct.JwtClaims.html), but could be
 /// generated from an external source.
-#[derive(Clone, Debug, PartialEq, Serialize, Deserialize, Hash)]
+#[derive(Clone, Debug, PartialEq, Eq, Serialize, Deserialize, Hash)]
 pub struct Jwt {
     jwt: String,
     header: jsonwebtoken::Header,
