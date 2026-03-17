f72d0d624c7b4af9bba2dce5bd679cf3660a1b37
Patrick Walsh <patrick.walsh@ironcorelabs.com>
2026-03-17 09:15:58 -0600

    Update shell syntax for newer tools

--- flake.nix
+++ flake.nix
@@ -7,19 +7,22 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, rust-overlay, flake-utils, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        overlays = [ (import rust-overlay) ];
-        pkgs = import nixpkgs { inherit system overlays; };
-        rusttoolchain =
-          pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
-      in rec {
-        # nix develop
-        devShell = pkgs.mkShell {
-          buildInputs = with pkgs;
-            [ rusttoolchain pkg-config openssl  ];
-        };
-
-      });
+  outputs = {
+    self,
+    nixpkgs,
+    rust-overlay,
+    flake-utils,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      overlays = [(import rust-overlay)];
+      pkgs = import nixpkgs {inherit system overlays;};
+      rusttoolchain =
+        pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+    in rec {
+      # nix develop
+      devShells.default = pkgs.mkShell {
+        buildInputs = with pkgs; [rusttoolchain pkg-config openssl];
+      };
+    });
 }
